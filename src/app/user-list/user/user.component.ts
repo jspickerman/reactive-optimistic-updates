616cd96b9ff1a1842d561a1837f31b5f87c5b5ea
jspickerman <jonathanspickerman@gmail.com>
2021-03-02 14:57:29 -0600

    1 file modified

--- src/app/user-list/user/user.component.ts
+++ src/app/user-list/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { User } from 'src/app/services/user.service';
+import { User } from '../../services/user.service';
 
 @Component({
   selector: 'app-user',
