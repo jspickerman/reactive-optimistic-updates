2499fa2e0693b0726a2d8fd0722bc7949ad6a8fb
jspickerman <jonathanspickerman@gmail.com>
2021-04-30 15:40:07 -0500

    1 file modified

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -39,32 +39,31 @@ export class UserListComponent implements OnInit {
       map((res: ApiResponse) => !(res.data))
     );
 
-    const newUserResponse$ = this.addUser$.pipe(
-      mergeMap((newUser: User) => this.userService.updateUser(newUser))
-    );
+    // const newUserResponse$ = this.addUser$.pipe(
+    //   mergeMap((newUser: User) => this.userService.updateUser(newUser))
+    // );
 
-    const newUser$ = newUserResponse$.pipe(
-      map((res: ApiResponse) => res.data)
-    );
+    // const newUser$ = newUserResponse$.pipe(
+    //   map((res: ApiResponse) => res.data)
+    // );
 
-    this.updateError$ = newUserResponse$.pipe(
-      map((res: ApiResponse) => !(res.error)),
-      startWith(false)
-    )
+    // this.updateError$ = newUserResponse$.pipe(
+    //   map((res: ApiResponse) => !(res.error)),
+    //   startWith(false)
+    // )
 
     const optimisticUsers$: Observable<User[]> = this.addUser$.pipe(
       withLatestFrom(latestApiUsers$),
       map(([newUser, users]) => {
         console.log('latest: ', users);
         return [...users, newUser];
-      }),
-      tap((opt) => console.log('optimistic: ', opt))
+      })
     );
 
-    const newUsers$: Observable<User[]> = combineLatest([newUser$, optimisticUsers$]).pipe(
-      tap(([newUser, optimisticUsers]) => console.log('new user: ', optimisticUsers)),
-      // map(([newUser, optimisticUsers]) => optimisticUsers.map((user) => !user.id && user.name === newUser.name ? newUser : user))
-    )
+    // const newUsers$: Observable<User[]> = combineLatest([newUser$, optimisticUsers$]).pipe(
+    //   tap(([newUser, optimisticUsers]) => console.log('new user: ', optimisticUsers)),
+    //   // map(([newUser, optimisticUsers]) => optimisticUsers.map((user) => !user.id && user.name === newUser.name ? newUser : user))
+    // )
 
     this.loaded$ = apiResponse$.pipe(
       map(() => true)
